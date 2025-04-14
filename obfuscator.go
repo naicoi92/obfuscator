477ac2f492c7c3c9fd7acc39381347ba667bb108
Nai Coi <naicoi@lttech.vn>
2025-04-15 00:49:57 +0700

    feat: add JavaScript obfuscation functionality
    This commit adds a new package `obfuscator` that provides a simple
    interface for obfuscating JavaScript code using the
    `javascript-obfuscator` library. The package includes the following
    changes:
    
    - Adds the `go.mod` and `go.sum` files to manage dependencies
    - Implements the `Obfuscator` struct to encapsulate the V8 isolate and
      context
    - Loads the `obfuscation.js` script that contains the
      `javascript-obfuscator` library
    - Provides methods to initialize the obfuscator, check the loaded
      library, and set up default obfuscation options
    - Implements the `Obfuscate` method to obfuscate the input JavaScript
      code and return the obfuscated result

---
+++ obfuscator.go
@@ -0,0 +1,108 @@
+package obfuscator
+
+import (
+	_ "embed"
+	"fmt"
+
+	"rogchap.com/v8go"
+)
+
+//go:embed obfuscation.js
+var JsCode string
+
+type Obfuscator struct {
+	Isolate *v8go.Isolate
+	Context *v8go.Context
+}
+
+func NewObfuscator() (*Obfuscator, error) {
+	o := &Obfuscator{}
+	o.Isolate = v8go.NewIsolate()
+	o.Context = v8go.NewContext(o.Isolate)
+	if err := o.Init(); err != nil {
+		return nil, err
+	}
+	return o, nil
+}
+
+func (o *Obfuscator) Close() {
+	o.Isolate.Dispose()
+	o.Context.Close()
+}
+
+func (o *Obfuscator) Init() error {
+	if err := o.SetupJSCode(); err != nil {
+		return err
+	}
+	if err := o.CheckJSCode(); err != nil {
+		return err
+	}
+	if err := o.SetupOptions(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (o *Obfuscator) CheckJSCode() error {
+	val, err := o.Context.RunScript("typeof JavaScriptObfuscator", "check.js")
+	if err != nil {
+		return err
+	}
+	if val.String() != "function" {
+		return fmt.Errorf("JavaScriptObfuscator is not defined")
+	}
+	return nil
+}
+
+func (o *Obfuscator) SetupJSCode() error {
+	code := fmt.Sprintf(`
+  (function() {
+    var self = this;
+    var window = this;
+    var module = {};
+    var exports = {};
+    module.exports = exports;
+    %s
+    globalThis.JavaScriptObfuscator = module.exports;
+	})()
+  `, JsCode)
+	if _, err := o.Context.RunScript(code, "obfuscation.js"); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (o *Obfuscator) SetupOptions() error {
+	code := `
+  const options = {
+      compact: (Math.random() < 0.5),
+      controlFlowFlattening: true,
+      controlFlowFlatteningThreshold: 1,
+      numbersToExpressions: true,
+      simplify: true,
+      stringArrayShuffle: true,
+      splitStrings: true,
+      stringArrayThreshold: 1
+  }
+  `
+	if _, err := o.Context.RunScript(code, "options.js"); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (o *Obfuscator) Obfuscate(code string) (string, error) {
+	codeString := fmt.Sprintf(
+		"const code = `%s`;const obfuscatedCode = JavaScriptObfuscator.obfuscate(code, options).getObfuscatedCode();obfuscatedCode;",
+		code,
+	)
+	val, err := o.Context.RunScript(codeString, "obfuscate.js")
+	if err != nil {
+		return "", err
+	}
+	obfuscatedCode := val.String()
+	if obfuscatedCode == "" {
+		return "", fmt.Errorf("obfuscated code is empty")
+	}
+	return obfuscatedCode, nil
+}
